6d16f80f21806d97eae155efef84be0710b74f04
aftiyah <abd2010ly@gmail.com>
2020-04-22 18:35:44 +0200

    handling error for books authors

--- src/Components/BookBottom.js
+++ src/Components/BookBottom.js
@@ -3,7 +3,7 @@ import React from 'react';
 const bookBottom = props => (
   <React.Fragment>
     <div className="book-title">{props.book.title}</div>
-    <div className="book-authors">{[...props.book.authors].join(', ')}</div>
+    <div className="book-authors">{props.book.authors && [...props.book.authors].join(', ')}</div>
   </React.Fragment>
 );
 
